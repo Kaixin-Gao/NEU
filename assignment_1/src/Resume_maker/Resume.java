284362b70f672419f8fb88374d0565078eec1ef6
kaixin gao <gao.k@husky.neu.edu>
2016-09-14 01:49:57 -0400

    begin this afternoon

---
+++ assignment_1/src/Resume_maker/Resume.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resume_maker;
+
+/**
+ *
+ * @author Max
+ */
+public class Resume {
+   
+    private String firstname;
+    private String lastname;
+    private String midname;
+    private String addressline1;
+    private String addressline2;
+    private String city;
+    private String zip;
+    private String country;
+    private String affiliation;
+    private String email;
+    private String phone;
+    private String area;
+    private String career;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    private String addressline1;
+    
+    
+    
+}
